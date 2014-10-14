73c207154438bc2b66cab964e6ac67ae45ca10cc
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-10-14 14:47:13 +0200

    [TASK] Added dedicated decorator

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var conductor = require( "./lib/transaction/conductor" );
 var webSockets = require( "./lib/transaction/websockets" );
 
-var typeFactory = require( "./lib/type/typefactory" );
-var TypeHelper = require( "./lib/type/typehelper" );
-var TypeInfo = require( "./lib/type/typeinfo" );
+var TypeDecorator = require( "./lib/type/decorator" );
+var typeFactory = require( "./lib/type/factory" );
+var TypeHelper = require( "./lib/type/helper" );
+var TypeInfo = require( "./lib/type/info" );
 
 module.exports = {
-	conductor   : conductor,
-	webSockets  : webSockets,
-	typeFactory : typeFactory,
-	TypeHelper  : TypeHelper,
-	TypeInfo    : TypeInfo
+	conductor     : conductor,
+	webSockets    : webSockets,
+	TypeDecorator : TypeDecorator,
+	typeFactory   : typeFactory,
+	TypeHelper    : TypeHelper,
+	TypeInfo      : TypeInfo
 };
